93ad709d61a02f691559096d6a0f835d1e792f14
PoffGenius <88441557+PoffGenius@users.noreply.github.com>
2021-08-18 05:49:42 +0000

    Authorization form project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Contact from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Contact />
+  </React.StrictMode>,
+  document.getElementById("app")
+);
